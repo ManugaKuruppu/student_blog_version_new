b10a6408aeb33d701d30be50b241230f96cbb63d
ManugaKuruppu <103434569+ManugaKuruppu@users.noreply.github.com>
2024-03-30 23:51:50 +0530

    update_new

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,4 @@
+<a href="/" class="block mx-auto mb-4">
+    <img src="{{asset('img/logonew.png')}}" class="h-35 w-40" alt="Logo">
+</a>
+
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,4 @@
+<a href="/" class="block mx-auto mb-4">
+    <img src="{{asset('img/logo.png')}}" class="w-50" alt="Logo">
+</a>
+
