83f9c084c76fc192e501d9761f41e7d2bfb3f148
ManugaKuruppu <103434569+ManugaKuruppu@users.noreply.github.com>
2024-03-31 23:14:35 +0530

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+
+        'user_id',
+        'title',
+        'slug',
+        'image',
+        'body',
+        'published_at',
+        'featured',
+    ];
 
     protected $casts = [
         'published_at' => 'datetime',
@@ -18,6 +31,10 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, );
     }
 
 
